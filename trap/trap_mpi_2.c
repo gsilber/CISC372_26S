247f2ccdb895546ac2ad854c7baf93df87b8a00c
Greg Silber <silber@udel.edu>
2026-02-18 20:05:59 -0500

    fixed up code

--- trap/trap_mpi_2.c
+++ trap/trap_mpi_2.c
@@ -5,6 +5,11 @@
 
 #define DEFAULT_NUM_TRAPS 20
 
+//getNumTraps: gets the number of trapezoids by requsting input on process 0 and broadcasting it to the other processes
+//Params: my_rank - the rank of the process, com_sz - the total number of processes, 
+// n_p - a pointer to store the number of trapezoids
+//Returns: void
+//NOTE: This function is different from the previous version because we are now using MPI to get the number of trapezoids from the user and broadcast it to all processes
 void getNumTraps(int my_rank,int com_sz, unsigned long long* n_p){
 	if (my_rank==0){
 		printf("Enter the number of trapezoids \n");
@@ -13,6 +18,11 @@ void getNumTraps(int my_rank,int com_sz, unsigned long long* n_p){
 	MPI_Bcast(n_p,1,MPI_LONG_LONG,0,MPI_COMM_WORLD);
 }
 
+//Trap: estimates the integral of func from start to end using the trapezoidal rule with trap_count trapezoids and base length base_len
+//Params: start - the start of the interval, end - the end of the interval,
+//trap_count - the number of trapezoids to use, base_len - the length of the base of each trapezoid
+//Returns: the estimated integral of func from start to end
+//NOTE: This function is unchanged from the serial version, we are just passing smaller intervals based on the rank of the process
 double Trap(double start, double end, unsigned long long trap_count,double base_len){
 	double approx=(func(start)+func(end))/2;
 	for (double i=1;i<trap_count;i++){
@@ -22,6 +32,8 @@ double Trap(double start, double end, unsigned long long trap_count,double base_
 	approx=base_len*approx;
 	return approx;
 }
+
+//Note: We are using MPI_Reduce to sum up the local approximations from each process instead of sending them to process 0 and summing them there. This is more efficient because it reduces the amount of communication between processes.
 int main(int argc,char** argv){
 	int my_rank,com_sz;
 	MPI_Init(NULL,NULL);
@@ -37,7 +49,15 @@ int main(int argc,char** argv){
 	double end=start+local_traps*h;
 	double local_approx=Trap(start,end,local_traps,h);
 	double approx=0;
-	MPI_Reduce(&local_approx,&approx,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+	//Do a tree reduction to sum up the local approximations from each process and store the result in approx on process 0
+	MPI_Reduce(
+	 &local_approx,  	//This is what we want to sum from each process.
+	 &approx,  		//This is the variable to store the result in.  The result will only be valid in process 0
+	 1, 				//Number of elements to send (in this case it is jsut a single double),
+	 MPI_DOUBLE,  	//The type of the data
+	 MPI_SUM,     	//The type of reduction we want
+	 0, 				//The process rank where approx will be populated by the reduction operation
+	 MPI_COMM_WORLD); //Same as always
 	if (my_rank==0){
 		printf("The ln(3) is approximately %.20f with %llu trapezoids\n",approx,traps);
 	}
--- trap/trap_serial.c
+++ trap/trap_serial.c
@@ -4,11 +4,18 @@
 
 #define DEFAULT_NUM_TRAPS 20
 
+//getNumTraps: gets the number of trapezoids to use from the command line, or returns a default value if none is given
+//Params: argc - the number of command line arguments, argv - the array of command line
+//Returns: the number of trapezoids to use
 unsigned long long getNumTraps(int argc,char** argv){
 	if (argc==1) return DEFAULT_NUM_TRAPS;
 	return atol(argv[1]);
 }
 
+//Trap: estimates the integral of func from start to end using the trapezoidal rule with trap_count trapezoids and base length base_len
+//Params: start - the start of the interval, end - the end of the interval,
+//trap_count - the number of trapezoids to use, base_len - the length of the base of each trapezoid
+//Returns: the estimated integral of func from start to end
 double Trap(double start, double end, unsigned long long trap_count,double base_len){
 	double approx=(func(start)+func(end))/2;
 	for (double i=1;i<trap_count;i++){
