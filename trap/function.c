29339457e032751cedf68a6b6c037c0199fc3d14
Greg Silber <silber@udel.edu>
2026-02-18 19:56:34 -0500

    cleaned

--- trap/function.c
+++ trap/function.c
@@ -1,4 +1,6 @@
 //the intergral of this function from 1-3 estimates the ln 3
+//Params: x - the value to be evaluated
+//Returns: the value of the function at x
 double func(double x){
 	return 1/x;
 }
--- trap/function.h
+++ trap/function.h
@@ -1,5 +1,9 @@
 
+//because our function must be evaluated at the endpoints of the interval, we define them here as constants
 #define a 1.0
 #define b 3.0
+
 //the intergral of this function from 1-3 estimates the ln 3
+//Params: x - the value to be evaluated
+//Returns: the value of the function at x
 double func(double x);
